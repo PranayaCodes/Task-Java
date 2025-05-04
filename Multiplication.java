603e30bb9b39a6be42640f4a359b90f291d3d2c2
PranayaCodes <khadkapranaya14@email.in>
2025-05-04 08:20:43 +0545

    Fourth push

---
+++ Multiplication.java
@@ -0,0 +1,15 @@
+public class Multiplication {
+    public static void printTables(){
+        for (int i =1;i<=10; i++){
+            System.out.println("Multiplication Table of "+i+":");
+            for (int j=1 ;j<=10;j++){
+                System.out.println(i+"X"+j+"="+(i*j));
+
+            }
+        }
+       
+    }
+    public static void main(String[] args){
+        printTables();
+    }
+}
---
+++ Square.java
@@ -0,0 +1,4 @@
+// Write a method that returns the sum of squares of digits of a number
+public class Square {
+    
+}
